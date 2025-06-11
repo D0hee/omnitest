f476d74eca2aca50a2066e02c5c62863dba30c7e
D0hee <pudge.nooob@gmail.com>
2025-06-11 11:52:36 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qncfhiganhvwtzvgjlwn.supabase.co'
+const supabaseUrl = 'https://jljhoeckswnxzjbzpmex.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
